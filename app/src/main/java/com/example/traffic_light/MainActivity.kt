60acae4dc0a62fd4e0c1bfefcec5ee66b99dd522
medvedevaviktoria <medvedevaviktoria176@gmail.com>
2025-05-08 21:41:14 +0300

    v2

--- app/src/main/java/com/example/traffic_light/MainActivity.kt
+++ app/src/main/java/com/example/traffic_light/MainActivity.kt
@@ -7,43 +7,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.traffic_light.databinding.ActivityMainBinding
 import com.example.traffic_light.enum.TrafficLight
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding: ActivityMainBinding
     //переменные для изображений
     var image_circle_red:ImageView? = null
     var image_circle_yellow:ImageView? = null
     var image_circle_green:ImageView? = null
-
     //переменные для работы кода
     private var current_light = TrafficLight.RED //начинаем показывать цвета с красного
     private var lastWasGreen: Boolean = false //если true - последним цветом перед жёлтым был зелёный, если false - последним цветом перед жёлтым был красный
-
     //переменные для изменения ориентации экрана
     private val STATE_CURRENT_LIGHT = "current_light"
     private val STATE_LAST_WAS_GREEN = "last_was_green"
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        image_circle_red = findViewById(R.id.image_circle_red)
-        image_circle_yellow = findViewById(R.id.image_circle_yellow)
-        image_circle_green = findViewById(R.id.image_circle_green)
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         //восстановление состояния после изменения ориентации экрана
         savedInstanceState?.let {
             current_light = TrafficLight.valueOf(it.getString(STATE_CURRENT_LIGHT).toString())
             lastWasGreen = it.getBoolean(STATE_LAST_WAS_GREEN)
-
         }
         updateLights()
     }
@@ -51,30 +38,27 @@ class MainActivity : AppCompatActivity() {
     fun onClickChangeColor(view: View) {
         changeColor()
         updateLights()
-
     }
-
 
     private fun updateLights() {
         when (current_light) {
             TrafficLight.RED -> {
-                image_circle_red?.setImageResource(R.drawable.circle_red)
-                image_circle_yellow?.setImageResource(R.drawable.circle_gray)
-                image_circle_green?.setImageResource(R.drawable.circle_gray)
+                binding.imageCircleRed.setImageResource(R.drawable.circle_red)
+                binding.imageCircleYellow.setImageResource(R.drawable.circle_gray)
+                binding.imageCircleGreen.setImageResource(R.drawable.circle_gray)
             }
             TrafficLight.YELLOW -> {
-                image_circle_red?.setImageResource(R.drawable.circle_gray)
-                image_circle_yellow?.setImageResource(R.drawable.circle_yellow)
-                image_circle_green?.setImageResource(R.drawable.circle_gray)
+                binding.imageCircleRed.setImageResource(R.drawable.circle_gray)
+                binding.imageCircleYellow.setImageResource(R.drawable.circle_yellow)
+                binding.imageCircleGreen.setImageResource(R.drawable.circle_gray)
             }
             TrafficLight.GREEN -> {
-                image_circle_red?.setImageResource(R.drawable.circle_gray)
-                image_circle_yellow?.setImageResource(R.drawable.circle_gray)
-                image_circle_green?.setImageResource(R.drawable.circle_green)
+                binding.imageCircleRed.setImageResource(R.drawable.circle_gray)
+                binding.imageCircleYellow.setImageResource(R.drawable.circle_gray)
+                binding.imageCircleGreen.setImageResource(R.drawable.circle_green)
             }
         }
     }
-
 
     private fun changeColor() {
         when (current_light) {
@@ -92,10 +76,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
         outState.putString(STATE_CURRENT_LIGHT, current_light.name)
         outState.putBoolean(STATE_LAST_WAS_GREEN, lastWasGreen)
     }
